5e9a6bf76be5951fa8a6a4454518e0876011d723
RaveenaWickramasinghe <raveenawickramasinghe@gmail.com>
2021-05-02 18:04:51 +0530

    First Commit to header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Navbar.css'
 
 export default class Navbar extends React.Component {
   constructor(props) {
@@ -9,27 +10,45 @@ export default class Navbar extends React.Component {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">ShopaFy</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link" aria-current="page" href="#">Products</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Cart</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">My Account</a>
-                </li>
-              </ul> 
-            </div>
+          <a class="navbar-brand" href="#">ShopaFy</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link waves-effect waves-light" href="#">
+                  <i class="fas fa-envelope"></i>Products
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link waves-effect waves-light" href="#">
+                  <i class="fas fa-gear"></i>Cart</a>
+              </li>
+              <div className="profile">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink-4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fas fa-user"></i>
+                  <img
+                    src="https://mdbootstrap.com/img/Photos/Avatars/img (31).jpg"
+                    className="rounded-circle"
+                    height="22"
+                      alt=""
+                      loading="lazy"
+                      /> </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
+                  <a class="dropdown-item waves-effect waves-light" href="#">My account</a>
+                  <a class="dropdown-item waves-effect waves-light" href="#">Log out</a>
+                </div>
+              </li>
+              </div>
+            </ul>
           </div>
-        </nav>
-      </div>
+    </nav>
+
+  </div>    
+        
+         
     )
   }
 }
